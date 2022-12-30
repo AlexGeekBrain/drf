477806c967f893224f31777590e1f7bc89987024
AlexGeekBrain <darkmilkyway@gmail.com>
2022-12-30 16:32:18 +0300

    Lesson_2

--- library/library/urls.py
+++ library/library/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from authors.views import AuthorModelViewSet
-from authapp.views import UserModelViewSet
+from users.views import UserModelViewSet
 
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
-router.register('authapp', UserModelViewSet)
+router.register('users', UserModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
